3a3296deb6f755b377b589c46892f4591ee5b43d
maureenmwangi <maureen.mwangi@student.moringaschool.com>
2022-02-06 03:11:20 +0300

    feature:optimized the functionality of my Button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 function myButton(){
     //code here
     //document.write("Your Results");
-    var correct = 0
-    var currentAnswer = document.first.answer1.answera;
-    if (currentAnswer=="James Gosling")(correct++);
-    document.write(correct);
+    var score = 0;
+    var currentAnswer1 = document.first.answer1.value;
+    var currentAnswer2 = document.first.answer2.value;
+    if (currentAnswer1=="James Gosling")(score++);
+    if (currentAnswer2=="Compares loose equality")(score++)
+    document.write(score);
 }
